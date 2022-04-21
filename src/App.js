b2c743336125376a281cd2ce776492b4de639218
Wenqian Jin <wenqian.jin1@grainger.com>
2022-04-20 21:38:11 -0500

    update note content

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
 import './App.css';
 
 function App() {console.log(1)
-  const [notes, setNotes] = React.useState([{note: 'Note1', id: nanoid(), content: '123'}, {note: 'Note2', id: nanoid(), content: '345'}]);
+  const [notes, setNotes] = React.useState([{note: 'Note1', id: nanoid(), content: ''}]);
   const [currentNote, setCurrentNote] = React.useState(notes[0].id);
   const selectNote = (e) => {
     setCurrentNote(e.target.id)
   }
   const addNote= () => {
     setNotes(prevNotes => [...prevNotes, {note: `Note${notes.length + 1}`, id: nanoid(), content: ''}])
+  }
+  const updateContent = (e) => {
+    console.log(e.target.value)
+    setNotes(prevNotes => {
+      return prevNotes.map(prevNote => {
+        return prevNote.id === currentNote ? {...prevNote, content: e.target.value} : prevNote
+      })
+    })
   }
 
   return (
@@ -23,7 +31,8 @@ function App() {console.log(1)
         addNote={addNote} />
       <Editor
         notes={notes}
-        currentNote={currentNote} />
+        currentNote={currentNote}
+        updateContent={updateContent} />
     </div>
   );
 }
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,13 +1,18 @@
 export default function Editor(props) {console.log(3);
   const text = props.notes.filter((note) => { return note.id === props.currentNote});
-  const textContent = text[0].content
+  const textContent = text[0].content;
+
   return (
     <section className="editor">
       <div className="editor-header">
         <h2>Content</h2>
       </div>
       <div className="editor-body">
-        <textarea rows="50" value={textContent}></textarea>
+        <textarea
+          rows="50"
+          value={textContent}
+          onChange={props.updateContent}>
+        </textarea>
       </div>
     </section>
   )
